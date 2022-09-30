7e898f482be9ad3c9c03d7a762edff126323bcfa
Adriano Santos <adriradiologista@hotmail.com.br>
2022-09-30 08:41:43 -0300

    Configurando o controller para recuperar os dados do repositório e adicionar no ModelAndView.

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -1,13 +1,37 @@
 package com.algaworks.festa.controller;
 
+import com.algaworks.festa.repositoty.Convidados;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * <h3>Classe responsável em receber a requisição e dizer o nome da view ao framework,
+ * para então ser renderizada de volta ao browser do cliente.</h3>
+ * <p>A anotação @Controller é para informar que a classe é um componente Spring e que é um controller.</p>
+ */
 @Controller
 public class ConvidadosController {
 
+    /**
+     * A anotação @Autowired no objeto convidados, informa que o Spring será responsável por
+     * estânciar o objeto quando for chamado aqui na classe.
+     */
+    @Autowired
+    private Convidados convidados;
+
+    /**
+     * Método responsáel por listar os convidados para mostrar na view.
+     * A anotação @GetMapping informa que responderá à requisição HTTP do tipo GET deve cair nesse método.
+     * Com isso, o Spring MVC sabe que a view deve ser renderizada para o cliente.
+     *
+     * @return Retorna a lista de convidados.
+     */
     @GetMapping("/convidados")
-    public String listar() {
-        return "ListaConvidados";
+    public ModelAndView listar() {
+        ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+        modelAndView.addObject("convidados", convidados.findAll());
+        return modelAndView;
     }
 }
