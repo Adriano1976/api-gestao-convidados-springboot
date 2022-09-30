ea72a49a958684e6de8aaf16e104cdd19ccfe2af
Adriano Santos <adriradiologista@hotmail.com.br>
2022-09-30 16:44:39 -0300

    configuração de teste.

---
+++ src/main/java/com/algaworks/festa/config/TextConfig.java
@@ -0,0 +1,31 @@
+package com.algaworks.festa.config;
+
+import com.algaworks.festa.model.Convidado;
+import com.algaworks.festa.repositoty.ConvidadosRepositoty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TextConfig implements CommandLineRunner {
+
+    @Autowired
+    private ConvidadosRepositoty convidadosRepositoty;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Convidado convidado = new Convidado(null, "Roberto", "dos Santos", 2);
+        Convidado convidado1 = new Convidado(null, "Marcos Gomes", "Gama", 0);
+        Convidado convidado2 = new Convidado(null, "Adriano", "Santos", 4);
+        Convidado convidado3 = new Convidado(null, "Josevaldo", "Rodrigues", 9);
+        Convidado convidado4 = new Convidado(null, "Karla", "dos Santos", 2);
+
+        convidadosRepositoty.saveAll(Arrays.asList(convidado, convidado1, convidado2, convidado3, convidado4));
+
+    }
+}
