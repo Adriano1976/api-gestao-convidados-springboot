022e3a0b31c3d8d8cf000e08aa433dd85e1afe24
Adriano Santos <adriradiologista@hotmail.com.br>
2022-09-29 22:29:00 -0300

    Criando projeto gestão de convidados.

--- src/main/java/com/algaworks/festa/GestaoFestaApplication.java
+++ src/main/java/com/algaworks/festa/GestaoFestaApplication.java
@@ -3,6 +3,10 @@ package com.algaworks.festa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe responsável pelo ponto de partida para a procura dos demais componentes da aplicação.
+ * A anotação @SpringBootApplication informa que a classe faz parte da configuração do Spring.
+ */
 @SpringBootApplication
 public class GestaoFestaApplication {
 
